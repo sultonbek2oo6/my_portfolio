ca85d4030b4112966b53f1a51f640d225fe53c62
sultonbek <h@MacBook-Air-M2.local>
2026-06-26 13:37:16 +0500

    api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,5 @@
-export const apiBase = import.meta.env.VITE_API_BASE || "http://localhost:5001";
+// Local localhost o'rniga Render-dagi jonli backend havolasini yozdik
+export const apiBase = import.meta.env.VITE_API_BASE || "https://my-portfolio-b5jo.onrender.com";
 
 export function authHeaders() {
   const token = localStorage.getItem("token");
